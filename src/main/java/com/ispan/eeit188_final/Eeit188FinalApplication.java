584f269571b032e8473f5be78a2c1e3cfb01bf53
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-04 19:27:54 +0800

    Rebuild...

---
+++ src/main/java/com/ispan/eeit188_final/Eeit188FinalApplication.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit188_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eeit188FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eeit188FinalApplication.class, args);
+	}
+
+}
