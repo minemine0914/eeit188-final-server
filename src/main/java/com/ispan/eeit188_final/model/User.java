a1669c0c10bdcf599c3a1e28b62256496bfb164a
CHIANGMENGHUA <max52116@gmail.com>
2024-09-04 16:19:17 +0800

    Build User, UserCollection, ChatRecord Entity 2024/9/4 16:19

---
+++ src/main/java/com/ispan/eeit188_final/model/User.java
@@ -0,0 +1,85 @@
+package com.ispan.eeit188_final.model;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "[user]")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "age")
+    private Short age;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "mobile_phone")
+    private String mobilePhone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "about")
+    private String about;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatetedAt;
+
+    @Column(name = "headshot_image_base64")
+    private String headshotImageBase64;
+
+    @Lob
+    @Column(name = "background_image_blob")
+    private byte[] backgroundImageBlob;
+
+    @PrePersist
+    public void onCreate() {
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        this.createdAt = currentTime;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatetedAt = new Timestamp(System.currentTimeMillis());
+    }
+}
