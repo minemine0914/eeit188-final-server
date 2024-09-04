ec7ff1f44bb60f60468e24d3aa4c46d514a13248
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-04 14:05:51 +0800

    Change name

--- src/main/java/com/ispan/eeit188/FinalApplication.java
+++ src/main/java/com/ispan/eeit188/FinalApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.ispan.eeit188;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/ispan/eeit188/controller/testController.java
+++ src/main/java/com/ispan/eeit188/controller/testController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.ispan.eeit188.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
