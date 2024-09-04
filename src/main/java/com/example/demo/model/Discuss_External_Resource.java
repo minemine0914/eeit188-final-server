044245e01eba237fe58e81a6db84660e782c226b
VJKenjo <mit123000222@gmail.com>
2024-09-04 15:33:40 +0800

    Discuss_External_Resource.java

--- src/main/java/com/example/demo/model/Discuss_External_Resource.java
+++ src/main/java/com/example/demo/model/Discuss_External_Resource.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Setter
 @Getter
 @Entity
