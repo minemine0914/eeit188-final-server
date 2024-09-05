8f8f05f3e8ee8ba802450559f9be3f2703ff31ff
VJKenjo <mit123000222@gmail.com>
2024-09-05 09:16:05 +0800

    Discuss_External_Resource.java

--- src/main/java/com/example/demo/model/Discuss_External_Resource.java
+++ src/main/java/com/example/demo/model/Discuss_External_Resource.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +20,8 @@ import lombok.Setter;
 @Table(name = "Discuss_External_Resource") // 討論區外部資源
 public class Discuss_External_Resource {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ur", columnDefinition = "VARCHAR(MAX)")
     private String ur;
 
