51ff66cc324a8dde39c71315ba93c0c0576f6561
VJKenjo <mit123000222@gmail.com>
2024-09-05 17:18:04 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/model/Housed_External_ResourceRepository.java
@@ -0,0 +1,8 @@
+package com.ispan.eeit188_final.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface Housed_External_ResourceRepository extends JpaRepository<Chat_External_Resource, UUID> {
+
+}
