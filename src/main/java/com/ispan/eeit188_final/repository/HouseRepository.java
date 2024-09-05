2ef1b0ac5d428e7786df687bcf30fa1141883dc3
MineMine <59837822+minemine0914@users.noreply.github.com>
2024-09-05 09:47:26 +0800

    Create HouseRepository.java

---
+++ src/main/java/com/ispan/eeit188_final/repository/HouseRepository.java
@@ -0,0 +1,9 @@
+package com.ispan.eeit188_final.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ispan.eeit188_final.model.House;
+
+public interface HouseRepository extends JpaRepository<House, String> {
+
+}
