47afde7745cf23b0e8b4e24957b432335dfce68e
VJKenjo <mit123000222@gmail.com>
2024-09-05 11:30:36 +0800

    Housed_External_Resource.java

--- src/main/java/com/example/demo/model/Housed_External_Resource.java
+++ src/main/java/com/example/demo/model/Housed_External_Resource.java
@@ -22,11 +22,14 @@ public class Housed_External_Resource {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ur", columnDefinition = "VARCHAR(MAX)")
-    private String ur;
+    @Column(name = "Id", columnDefinition = "UUID")
+    private UUID Id;
 
     @Column(name = "house_id", columnDefinition = "UUID")
     private UUID HouseId;
+
+    @Column(name = "url", columnDefinition = "VARCHAR(MAX)")
+    private String url;
 
     @Column(name = "type", columnDefinition = "VARCHAR(10)")
     private String type;
