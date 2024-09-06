fefce1a4eaf3f363a297af942ce630a3f1e93cb1
Jc120 <71688729+001200jc@users.noreply.github.com>
2024-09-06 20:32:40 +0800

    add controller

--- src/main/java/com/ispan/eeit188_final/controller/CouponController.java
+++ src/main/java/com/ispan/eeit188_final/controller/CouponController.java
@@ -1,6 +1,52 @@
 package com.ispan.eeit188_final.controller;
 
+import com.ispan.eeit188_final.model.Coupon;
+import com.ispan.eeit188_final.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/coupons")
 public class CouponController {
-    
-    
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping
+    public ResponseEntity<String> createCoupon(@RequestBody String jsonRequest) {
+        return couponService.createCoupon(jsonRequest);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getCoupon(@PathVariable UUID id) {
+        return couponService.getCoupon(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Coupon>> getAllCoupons() {
+        return ResponseEntity.ok(couponService.getAllCoupons());
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Coupon>> getCouponsByUserId(@PathVariable UUID userId) {
+        return ResponseEntity.ok(couponService.getCouponsByUserId(userId));
+    }
+
+    @GetMapping("/house/{houseId}")
+    public ResponseEntity<List<Coupon>> getCouponsByHouseId(@PathVariable UUID houseId) {
+        return ResponseEntity.ok(couponService.getCouponsByHouseId(houseId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCoupon(@PathVariable UUID id, @RequestBody String jsonRequest) {
+        return couponService.updateCoupon(id, jsonRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCoupon(@PathVariable UUID id) {
+        return couponService.deleteCoupon(id);
+    }
 }
--- src/main/java/com/ispan/eeit188_final/controller/DiscussController.java
+++ src/main/java/com/ispan/eeit188_final/controller/DiscussController.java
@@ -1,6 +1,48 @@
 package com.ispan.eeit188_final.controller;
 
+import com.ispan.eeit188_final.model.Discuss;
+import com.ispan.eeit188_final.service.DiscussService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/discuss")
 public class DiscussController {
-    
-    
+
+    @Autowired
+    private DiscussService discussService;
+
+    @PostMapping
+    public ResponseEntity<String> createDiscuss(@RequestBody String jsonRequest) {
+        return discussService.createDiscuss(jsonRequest);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getDiscuss(@PathVariable UUID id) {
+        return discussService.getDiscuss(id);
+    }
+
+    @GetMapping("/house/{houseId}")
+    public ResponseEntity<List<Discuss>> getDiscussionsByHouseId(@PathVariable UUID houseId) {
+        return ResponseEntity.ok(discussService.getDiscussionsByHouseId(houseId));
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Discuss>> getDiscussionsByUserId(@PathVariable UUID userId) {
+        return ResponseEntity.ok(discussService.getDiscussionsByUserId(userId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateDiscuss(@PathVariable UUID id, @RequestBody String jsonRequest) {
+        return discussService.updateDiscuss(id, jsonRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDiscuss(@PathVariable UUID id) {
+        return discussService.deleteDiscuss(id);
+    }
 }
