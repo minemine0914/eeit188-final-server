5eb7a026aaa6f16db34878f492d2fdda430ecd23
VJKenjo <mit123000222@gmail.com>
2024-09-06 10:55:35 +0800

    Housed_External_ResourceRepository.java

--- src/main/java/com/example/demo/model/Housed_External_ResourceRepository.java
+++ src/main/java/com/example/demo/model/Housed_External_ResourceRepository.java
@@ -3,6 +3,6 @@ package com.ispan.eeit188_final.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface Housed_External_ResourceRepository extends JpaRepository<Chat_External_Resource, UUID> {
+public interface Housed_External_ResourceRepository extends JpaRepository<Housed_External_Resource, UUID> {
 
 }
