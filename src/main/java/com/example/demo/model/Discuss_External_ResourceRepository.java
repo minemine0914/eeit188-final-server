630e91dabba200c63cf0a347bcb2569c9ab7e29b
VJKenjo <mit123000222@gmail.com>
2024-09-06 10:55:15 +0800

    Discuss_External_ResourceRepository.java

--- src/main/java/com/example/demo/model/Discuss_External_ResourceRepository.java
+++ src/main/java/com/example/demo/model/Discuss_External_ResourceRepository.java
@@ -3,6 +3,6 @@ package com.ispan.eeit188_final.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface Discuss_External_ResourceRepository extends JpaRepository<Chat_External_Resource, UUID> {
+public interface Discuss_External_ResourceRepository extends JpaRepository< Discuss_External_Resource, UUID> {
 
 }
