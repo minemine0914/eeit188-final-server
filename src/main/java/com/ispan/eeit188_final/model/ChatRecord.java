875c2e7579de2803f8ae56457a2b102c9d177e19
蔣孟華 (Leon Chiang) <50078157+CHIANGMENGHUA@users.noreply.github.com>
2024-09-07 17:43:07 +0800

    Add relationship

--- src/main/java/com/ispan/eeit188_final/model/ChatRecord.java
+++ src/main/java/com/ispan/eeit188_final/model/ChatRecord.java
@@ -3,6 +3,9 @@ package com.ispan.eeit188_final.model;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -50,6 +54,10 @@ public class ChatRecord {
 
     @Column(name = "created_at", columnDefinition = "datetime2")
     private Timestamp createdAt;
+
+    // Relationship
+    @OneToMany(mappedBy = "chat_record", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatExternalResource> chatExternalResources;
 
     // Methods
     @PrePersist
--- src/main/java/com/ispan/eeit188_final/model/User.java
+++ src/main/java/com/ispan/eeit188_final/model/User.java
@@ -3,13 +3,17 @@ package com.ispan.eeit188_final.model;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.UUID;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -75,6 +79,31 @@ public class User {
     @Lob
     @Column(name = "background_image_blob", columnDefinition = "varbinary(max)")
     private byte[] backgroundImageBlob;
+
+    // Relationship
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cart> carts;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ticket> tickets;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TransactionRecord> transactionRecords;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Coupon> coupons;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<House> houses;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserCollection> userCollections;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Discuss> discusses;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatRecord> chatRecords;
 
     @PrePersist
     public void onCreate() {
