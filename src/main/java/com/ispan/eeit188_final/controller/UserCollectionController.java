43a494100e375f3cb4bd6aa4933840e19fe253c8
蔣孟華 (Leon Chiang) <50078157+CHIANGMENGHUA@users.noreply.github.com>
2024-09-10 09:40:57 +0800

    Add @CrossOrigin in controller

--- src/main/java/com/ispan/eeit188_final/controller/UserCollectionController.java
+++ src/main/java/com/ispan/eeit188_final/controller/UserCollectionController.java
@@ -2,6 +2,7 @@ package com.ispan.eeit188_final.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.ispan.eeit188_final.service.UserCollectionService;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user-collection")
 public class UserCollectionController {
 
