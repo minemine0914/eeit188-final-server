f0a46cf085d54dd2d86a03eecda7d225e1f7e712
strawberry1515 <straw15berry@gmail.com>
2024-09-26 16:33:35 +0800

    查詢房東

--- src/main/java/com/ispan/eeit188_final/controller/UserController.java
+++ src/main/java/com/ispan/eeit188_final/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.ispan.eeit188_final.controller;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
+import com.ispan.eeit188_final.model.User;
 import com.ispan.eeit188_final.service.UserService;
-
-import java.util.UUID;
 
 @RestController
 @CrossOrigin
@@ -37,6 +47,12 @@ public class UserController {
         return userService.getUsers(pageNo, pageSize);
     }
 
+    // 批量查詢房東
+    @GetMapping("/find-hosts")
+    public List<User> getHosts() {
+        return userService.findAllHost();
+    }
+    
     // 創建新用戶
     @PostMapping("/createUser")
     public ResponseEntity<String> createUser(
